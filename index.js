ec542a48c401da2d9c1bae4d6a0aaffb22ef0473
Rashid Khan <github.fliplap@spamgourmet.com>
2019-03-26 13:42:30 -0700

    Accept when addressed to multiple recipients

--- index.js
+++ index.js
@@ -1,68 +1,32 @@
 // smtp.js
 const { SMTPServer } = require("smtp-server");
+const processEmail = require("./process");
 const { simpleParser } = require("mailparser");
 const config = require("./poopmail.json");
-
-const sendmail = require("sendmail")({ silent: !config.debug });
-const plugins = require("./plugins");
-
 const server = new SMTPServer({
   // disable STARTTLS to allow authentication in clear text mode
   disabledCommands: ["STARTTLS", "AUTH"],
   logger: config.debug,
   onData(stream, session, callback) {
+    callback(null, "Accepted"); // We accept everything
+
     simpleParser(stream).then(parsed => {
       const { from, to, subject, html, text, attachments } = parsed;
-      const address = to.value[0].address;
 
       if (config.debug) {
         console.log("INCOMING EMAIL", JSON.stringify(parsed, null, " "));
       }
 
-      const incoming = {
-        from: from.value[0].address,
-        to: to.value[0].address,
-        subject,
-        text,
-        html,
-        attachments
-      };
-
-      const outgoing = {
-        from: `Forwarded for ${address} <noreply@poopmail>`,
-        to: config.to,
-        subject,
-        text,
-        html,
-        attachments: attachments || []
-      };
-
-      function act(decision) {
-        if (decision === true) {
-          sendmail(outgoing);
-          if (config.debug) console.log("FORWARDED EMAIL", address);
-          return "Accepted";
-        }
-        if (decision === false) {
-          if (config.debug) {
-            console.log(
-              "REJECTED EMAIL",
-              `to ${address} from ${from.value[0].address}`
-            );
-          }
-          return new Error("Rejected");
-        }
-      }
-
-      for (let plugin of plugins) {
-        const decision = plugin(incoming);
-        if (decision === true || decision === false) {
-          return callback(null, act(outgoing));
-        }
-      }
-
-      // No decision
-      callback(null, act(config.default_accept));
+      to.value.forEach(recp =>
+        processEmail({
+          from: from.value[0].address,
+          to: recp.address,
+          subject,
+          text,
+          html,
+          attachments
+        })
+      );
     });
   }
 });
---
+++ process.js
@@ -0,0 +1,40 @@
+const config = require("./poopmail.json");
+const sendmail = require("sendmail")({ silent: !config.debug });
+const plugins = require("./plugins");
+
+module.exports = incoming => {
+  const address = incoming.to;
+  const { subject, text, html, attachments, from } = incoming;
+
+  const outgoing = {
+    from: `Forwarded for ${address} <${from}>`,
+    to: config.to,
+    subject,
+    text,
+    html,
+    attachments: attachments || []
+  };
+
+  function act(decision) {
+    if (decision === true) {
+      sendmail(outgoing);
+      if (config.debug) console.log("FORWARDED EMAIL", address);
+    }
+    if (decision === false) {
+      if (config.debug) {
+        console.log("REJECTED EMAIL", `to ${address} from ${from}`);
+      }
+    }
+    return decision;
+  }
+
+  for (let plugin of plugins) {
+    const decision = plugin(incoming);
+    if (decision === true || decision === false) {
+      return act(decision);
+    }
+  }
+
+  // No decision
+  return act(config.default_accept);
+};
